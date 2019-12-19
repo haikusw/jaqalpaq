69fe8b5768f980c33c6aa10b6d9b5ac355e7876d
B. C. A. Morrison <benmorrison@unm.edu>
2019-12-19 13:43:55 -0700

    Replace reference to QASM with reference to Jaqal.

--- qscout/quil/qscoutam.py
+++ qscout/quil/qscoutam.py
@@ -2,6 +2,6 @@ from pyquil.api import QAM
 
 class QSCOUTAM(QAM):
 	def load(self, executable):
-		raise QSCOUTError("QSCOUT cannot run programs through the Quil API. Generate a QASM file with compile() and submit it directly to Peter Maunz.")
+		raise QSCOUTError("QSCOUT cannot run programs through the Quil API. Generate a Jaqal file with compile() and submit it directly to Dr. Peter Maunz.")
 	def run(self):
-		raise QSCOUTError("QSCOUT cannot run programs through the Quil API. Generate a QASM file with compile() and submit it directly to Peter Maunz.")
+		raise QSCOUTError("QSCOUT cannot run programs through the Quil API. Generate a Jaqal file with compile() and submit it directly to Dr. Peter Maunz.")
