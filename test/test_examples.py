460a818b755cceb392a05854c58d3cd91b317239
Peter Maunz <plmaunz@sandia.gov>
2019-12-19 23:22:07 -0700

    fix hadamard example and add test to parse the examples

--- test/test_examples.py
+++ test/test_examples.py
@@ -2,6 +2,8 @@ import unittest
 from pathlib import Path
 from iqasm.testing.mixin import ParserTesterMixin
 
+from iqasm.iter_gates import parse_unitary_timed_gates
+
 top_example_dir = Path('examples')
 test_example_dir = Path('../examples')
 
@@ -19,9 +21,10 @@ class ExampleFileTester(ParserTesterMixin, unittest.TestCase):
         text = Path(file_name).read_text()
         parser = self.make_parser()
         try:
-            parser.parse(text)
+            tree = parser.parse(text)
         except Exception as e: 
             raise AssertionError(f"{file_name}: {e}")
+        return tree
 
     def test_hadamard(self):
         """Test Hadamard example"""
@@ -30,3 +33,16 @@ class ExampleFileTester(ParserTesterMixin, unittest.TestCase):
     def test_gst(self):
         """Test Single Qubit GST example"""
         self.implement_file_test(example_dir / "single_qubit_gst.xqasm")
+
+    def test_hadamard_unitary_timed_gates(self):
+        """Test visiting the hadamard program"""
+        file_path = example_dir / "hadamard.xqasm"
+        text = Path(file_path).read_text()
+        parse_unitary_timed_gates(text)
+        
+    def test_gst_unitary_timed_gates(self):
+        """Test visiting the gst program"""
+        file_path = example_dir / "single_qubit_gst.xqasm"
+        text = Path(file_path).read_text()
+        parse_unitary_timed_gates(text)
+ 
