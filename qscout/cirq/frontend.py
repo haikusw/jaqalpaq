Fix radian/degree confusion and a couple of phase factors.

qscout/cirq/frontend.py
@@ -4,11 +4,11 @@ from cirq import XXPowGate, XPowGate, YPowGate, ZPowGate, PhasedXPowGate
 import numpy as np
 
 CIRQ_NAMES = {
-	XXPowGate: (lambda g: ('MS', 0, g.exponent * np.pi / 2)),
-	XPowGate: (lambda g: ('R', 0, g.exponent * np.pi)),
-	YPowGate: (lambda g: ('R', np.pi/2, g.exponent * np.pi)),
-	ZPowGate: (lambda g: ('Rz', g.exponent * np.pi)),
-	PhasedXPowGate: (lambda g: ('R', g.phase_exponent * np.pi, g.exponent * np.pi))
+	XXPowGate: (lambda g: ('MS', 0, g.exponent * 180.0)),
+	XPowGate: (lambda g: ('R', 0, g.exponent * 180.0)),
+	YPowGate: (lambda g: ('R', 90.0, g.exponent * 180.0)),
+	ZPowGate: (lambda g: ('Rz', g.exponent * 180.0)),
+	PhasedXPowGate: (lambda g: ('R', g.phase_exponent * 180.0, g.exponent * 180.0))
 }
 
 def qscout_circuit_from_cirq_circuit(ccirc):

qscout/qiskit/frontend.py
@@ -3,6 +3,7 @@ from qscout.core import ScheduledCircuit
 from qiskit.converters import dag_to_circuit
 from qscout import QSCOUTError
 #from sympy.core.evalf import N
+import numpy as np
 
 QISKIT_NAMES = {'i': 'I', 'r': 'R', 'sx': 'Sx', 'sy': 'Sy', 'x': 'Px', 'y': 'Py', 'rz': 'Rz'}
 
@@ -75,14 +76,14 @@ def qscout_circuit_from_qiskit_circuit(circuit):
 		elif instr[0].name in QISKIT_NAMES:
 			target = instr[1][0]
 			if target.register.name in qsc.registers:
-				block.append(qsc.build_gate(QISKIT_NAMES[instr[0].name], qsc.registers[target.register.name][target.index], *[float(param) for param in instr[0].params]))
+				block.append(qsc.build_gate(QISKIT_NAMES[instr[0].name], qsc.registers[target.register.name][target.index], *[float(param) * 180.0 / np.pi for param in instr[0].params]))
 			else:
 				raise QSCOUTError("Gate register %s invalid!" % target.register.name)
 		elif instr[0].name == 'ms':
 			targets = instr[1]
 			if targets[0].register.name in qsc.registers:
 				if targets[1].register.name in qsc.registers:
-					block.append(qsc.build_gate('MS', *[qsc.registers[target.register.name][target.index] for target in targets], *[float(param) for param in instr[0].params]))
+					block.append(qsc.build_gate('MS', *[qsc.registers[target.register.name][target.index] for target in targets], *[float(param) * 180.0 / np.pi for param in instr[0].params]))
 				else:
 					raise QSCOUTError("Gate register %s invalid!" % targets[1].register.name)
 			else:

qscout/quil/gates.py
@@ -6,8 +6,8 @@ def R(theta, phi):
 					[(-1j * np.cos(theta) + np.sin(theta)) * np.sin(phi / 2.0), np.cos(phi / 2.0)]])
 SX = RX(np.pi/2)
 SY = RY(np.pi/2)
-def MS(theta, phi): # TODO phase correctly
-	return np.array([[np.cos(theta), 0, 0, -1j * np.sin(theta)],
-					[0, np.cos(theta), -1j * np.sin(theta), 0],
-					[0, -1j * np.sin(theta), np.cos(theta), 0],
-					[-1j * np.sin(theta), 0, 0, np.cos(theta)]])
+def MS(theta, phi):
+	return np.array([[np.cos(theta/2.0), 0, 0, -1j * (np.cos(phi * 2.0) - 1j * np.sin(phi * 2.0)) * np.sin(theta/2.0)],
+					[0, np.cos(theta/2.0), -1j * np.sin(theta/2.0), 0],
+					[0, -1j * np.sin(theta/2.0), np.cos(theta/2.0), 0],
+					[-1j * (np.cos(phi * 2.0) - 1j * np.sin(phi * 2.0)) * np.sin(theta/2.0), 0, 0, np.cos(theta/2.0)]])

qscout/quil/ioncompiler.py
@@ -4,6 +4,7 @@ from pyquil.quil import Program, Gate
 from pyquil.quilbase import Measurement, ResetQubit, Reset
 from qscout.core import ScheduledCircuit
 from qscout import QSCOUTError
+import numpy as np
 
 QUIL_NAMES = {'I': 'I', 'R': 'R', 'SX': 'Sx', 'SY': 'Sy', 'X': 'Px', 'Y': 'Py', 'RZ': 'Rz', 'MS': 'MS'}
 
@@ -49,7 +50,7 @@ class IonCompiler(AbstractCompiler):
 					# measure_accumulator = set()
 			if isinstance(instr, Gate):
 				if instr.name in QUIL_NAMES:
-					qsc.gate(QUIL_NAMES[instr.name], *[qreg[qubit.index] for qubit in instr.qubits], *[float(p) for p in instr.params])
+					qsc.gate(QUIL_NAMES[instr.name], *[qreg[qubit.index] for qubit in instr.qubits], *[float(p) * 180 / np.pi for p in instr.params])
 				else:
 					raise QSCOUTError("Gate %s not in native gate set." % instr.name)
 			elif isinstance(instr, Reset):