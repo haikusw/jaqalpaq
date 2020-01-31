5aff415b7bba4ccb0e2f4e042488d9b9938cac24
B. C. A. Morrison <benmorrison@unm.edu>
2020-01-30 17:33:40 -0700

    Convert measurements, parallel gates correctly from cirq.

--- qscout/cirq/frontend.py
+++ qscout/cirq/frontend.py
@@ -1,6 +1,6 @@
 from qscout.core.circuit import ScheduledCircuit
 from qscout import QSCOUTError
-from cirq import XXPowGate, XPowGate, YPowGate, ZPowGate, PhasedXPowGate
+from cirq import XXPowGate, XPowGate, YPowGate, ZPowGate, PhasedXPowGate, MeasurementGate
 import numpy as np
 
 CIRQ_NAMES = {
@@ -19,7 +19,7 @@ def qscout_circuit_from_cirq_circuit(ccirc):
 	:rtype: ScheduledCircuit
 	:raises QSCOUTError: if the input contains any instructions other than ``cirq.XXPowGate``, ``cirq.XPowGate``, ``cirq.YPowGate``, ``cirq.ZPowGate``, or ``cirq.PhasedXPowGate``.
 	"""
-	qcirc = ScheduledCircuit(True)
+	qcirc = ScheduledCircuit(True) # TODO: Allow user to supply a different native gateset.
 	try:
 		n = 1 + max([qb.x for qb in ccirc.all_qubits()])
 		line = True
@@ -29,18 +29,26 @@ def qscout_circuit_from_cirq_circuit(ccirc):
 		qubitmap = {cqubits[i]: i for i in range(n)}
 		line = False
 	allqubits = qcirc.reg('allqubits', n)
-	qcirc.gate('prepare_all')
+	need_prep = True
 	for moment in ccirc:
+		if need_prep:
+			qcirc.gate('prepare_all')
+		if len(moment) == n and all([op.gate for op in moment]) and all([isinstance(op.gate, MeasurementGate) for op in moment]):
+			qcirc.gate('measure_all')
+			need_prep = True
+			continue
+		block = qcirc.block(parallel=True) # Note: If you tell Cirq you want MS gates in parallel, we'll generate a Jaqal file with exactly that, never mind that QSCOUT can't execute it.
 		for op in moment:
 			if op.gate:
 				if type(op.gate) in CIRQ_NAMES:
 					if line:
-						qcirc.gate(*CIRQ_NAMES[type(op.gate)](op.gate, *[allqubits[qb.x] for qb in op.qubits]))
+						block.append(qcirc.build_gate(*CIRQ_NAMES[type(op.gate)](op.gate, *[allqubits[qb.x] for qb in op.qubits])))
 					else:
-						qcirc.gate(*CIRQ_NAMES[type(op.gate)](op.gate, *[allqubits[qubitmap[qb]] for qb in op.qubits]))
+						block.append(qcirc.build_gate(*CIRQ_NAMES[type(op.gate)](op.gate, *[allqubits[qubitmap[qb]] for qb in op.qubits])))
 				else:
 					raise QSCOUTError("Convert circuit to ion gates before compiling.")
 			else:
-				raise QSCOUTError("Cannot compile operation %s." % op) # TODO: Support non-gate instructions.
-	qcirc.gate('measure_all')
+				raise QSCOUTError("Cannot compile operation %s." % op)
+	if not need_prep: # If we just measured, or the circuit is empty, don't add a final measurement.
+		qcirc.gate('measure_all')
 	return qcirc
