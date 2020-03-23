dd6cac801d9d7b921bbc5e7afee958313299a2d2
Antonio Russo <arusso@sandia.gov>
2020-03-23 09:47:24 -0600

    Track AbstractGate when creating a GateStatement

--- jaqalpup/core/gate.py
+++ jaqalpup/core/gate.py
@@ -4,15 +4,23 @@ class GateStatement:
 	"""
 	Represents a Jaqal gate statement.
 	
-	:param str name: The name of the gate to call.
+	:param GateDefinition gate_def: The gate to call.
 	:param dict parameters: A map from gate parameter names to the values to pass for those parameters. Can be omitted for gates that have no parameters.
 	"""
-	def __init__(self, name, parameters = None):
-		self.name = name
+	def __init__(self, gate_def, parameters = None):
+		self._gate_def = gate_def
 		if parameters is None:
 			self._parameters = {}
 		else:
 			self._parameters = parameters
+	
+	@property
+	def name(self):
+		return self._gate_def.name
+	
+	@property
+	def gate_def(self):
+		return self._gate_def
 	
 	@property
 	def parameters(self):
--- jaqalpup/core/gatedef.py
+++ jaqalpup/core/gatedef.py
@@ -71,7 +71,7 @@ class AbstractGate:
 			raise QSCOUTError("Cannot mix named and positional parameters in call to gate.")
 		for param in self.parameters:
 			param.validate(params[param.name])
-		return GateStatement(self.name, params)
+		return GateStatement(self, params)
 	
 	def __call__(self, *args, **kwargs):
 		self.call(*args, **kwargs)
