9d532e85638053c57b497c0c6408d1c7e9fa3a59
Jay <jayvand@sandia.gov>
2020-03-31 11:13:00 -0600

    Removed ability to set a random object in core tests. Just set the seed in setUp instead.

--- jaqalpup/core/test/common.py
+++ jaqalpup/core/test/common.py
@@ -1,18 +1,19 @@
+import random
+
 from jaqalpup.core import (
     Register, Constant, NamedQubit, PARAMETER_TYPES, Parameter,
 )
 from .randomize import (
-    random_identifier, random_whole, random_integer, resolve_random_instance
+    random_identifier, random_whole, random_integer
 )
 
 
-def make_random_parameter(allowed_types=None, rand=None, return_params=False):
+def make_random_parameter(allowed_types=None, return_params=False):
     """Return a parameter with a random type and name."""
-    rand = resolve_random_instance(rand)
-    name = random_identifier(rand=rand)
+    name = random_identifier()
     if allowed_types is None:
         allowed_types = PARAMETER_TYPES
-    param_type = rand.choice(allowed_types)
+    param_type = random.choice(allowed_types)
     if param_type not in PARAMETER_TYPES:
         raise ValueError(f"Unknown parameter type {param_type}")
     param = Parameter(name, param_type)
@@ -22,12 +23,12 @@ def make_random_parameter(allowed_types=None, rand=None, return_params=False):
         return param, name, param_type
 
 
-def make_random_register(name=None, size=None, rand=None, return_params=False):
+def make_random_register(name=None, size=None, return_params=False):
     """Make a random register"""
     if name is None:
-        name = random_identifier(rand=rand)
+        name = random_identifier()
     if size is None:
-        size = random_whole(rand=rand)
+        size = random_whole()
     reg = Register(name, size)
     if not return_params:
         return reg
@@ -35,12 +36,12 @@ def make_random_register(name=None, size=None, rand=None, return_params=False):
         return reg, name, size
 
 
-def make_random_size_constant(name=None, value=None, rand=None, return_params=False):
+def make_random_size_constant(name=None, value=None, return_params=False):
     """Make a random Constant that can represent a size"""
     if name is None:
-        name = random_identifier(rand=rand)
+        name = random_identifier()
     if value is None:
-        value = random_whole(rand=rand)
+        value = random_whole()
     const = Constant(name, value)
     if not return_params:
         return const
@@ -48,10 +49,10 @@ def make_random_size_constant(name=None, value=None, rand=None, return_params=Fa
         return const, name, value
 
 
-def choose_random_qubit_getitem(reg, index=None, rand=None, return_params=False):
+def choose_random_qubit_getitem(reg, index=None, return_params=False):
     """Choose a random qubit from a register with its getitem special method."""
     if index is None:
-        index = random_integer(lower=0, upper=reg.size - 1, rand=rand)
+        index = random_integer(lower=0, upper=reg.size - 1)
     qubit = reg[index]
     if not return_params:
         return qubit
@@ -59,12 +60,12 @@ def choose_random_qubit_getitem(reg, index=None, rand=None, return_params=False)
         return qubit, index
 
 
-def choose_random_qubit_init(reg, name=None, index=None, rand=None, return_params=False):
+def choose_random_qubit_init(reg, name=None, index=None, return_params=False):
     """Choose a random qubit from a register using the NamedQubit's constructor."""
     if index is None:
-        index = random_integer(lower=0, upper=reg.size - 1, rand=rand)
+        index = random_integer(lower=0, upper=reg.size - 1)
     if name is None:
-        name = random_identifier(rand=rand)
+        name = random_identifier()
     qubit = NamedQubit(name, reg, index)
     if not return_params:
         return qubit
@@ -76,10 +77,10 @@ def make_qubit_name(reg, index):
     return f"{reg.name}[{index}]"
 
 
-def make_map_full(reg, name=None, rand=None, return_params=False):
+def make_map_full(reg, name=None, return_params=False):
     """Make a map alias to the given register."""
     if name is None:
-        name = random_identifier(rand=rand)
+        name = random_identifier()
     map_reg = Register(name, alias_from=reg)
     if not return_params:
         return map_reg
@@ -87,12 +88,12 @@ def make_map_full(reg, name=None, rand=None, return_params=False):
         return map_reg, name
 
 
-def make_map_slice(reg, name=None, map_slice=None, rand=None, return_params=False):
+def make_map_slice(reg, name=None, map_slice=None, return_params=False):
     """Make a map alias to a slice of the given register."""
     if name is None:
-        name = random_identifier(rand=rand)
+        name = random_identifier()
     if map_slice is None:
-        map_slice = make_random_slice(reg.size, rand=rand)
+        map_slice = make_random_slice(reg.size)
     map_reg = Register(name, alias_from=reg, alias_slice=map_slice)
     if not return_params:
         return map_reg
@@ -100,10 +101,10 @@ def make_map_slice(reg, name=None, map_slice=None, rand=None, return_params=Fals
         return map_reg, name, map_slice
 
 
-def make_random_slice(upper, rand=None):
+def make_random_slice(upper):
     """Return a slice of an array with upper bound upper. Guaranteed to have
     at least one element."""
-    start = random_whole(upper=upper - 1, rand=rand)
-    length = random_whole(upper=(upper - start), rand=rand)
-    step = random_whole(upper=16, rand=rand)
+    start = random_whole(upper=upper - 1)
+    length = random_whole(upper=(upper - start))
+    step = random_whole(upper=16)
     return slice(start, start + length, step)
--- jaqalpup/core/test/randomize.py
+++ jaqalpup/core/test/randomize.py
@@ -10,47 +10,36 @@ start_chars = make_char_range('a', 'z') + make_char_range('A', 'Z') + ['_']
 continue_chars = start_chars + make_char_range('0', '9')
 
 
-def random_identifier(count=None, rand=None):
+def random_identifier(count=None):
     """Return a random identifier of the given length."""
-    rand = resolve_random_instance(rand)
     if count is None:
-        count = random_whole(rand=rand)
+        count = random_whole()
     else:
         if not isinstance(count, int):
             raise TypeError("Count must be an integer")
         if count < 1:
             raise ValueError("Count must be at least 1")
-    first_chars = rand.choices(start_chars, k=1)
+    first_chars = random.choices(start_chars, k=1)
     if count == 1:
         rest_chars = []
     else:
         assert count > 1, f"Bad count: {count}"
-        rest_chars = rand.choices(continue_chars, k=count - 1)
+        rest_chars = random.choices(continue_chars, k=count - 1)
     return ''.join(first_chars + rest_chars)
 
 
-def random_whole(*, lower=1, upper=100, rand=None):
+def random_whole(*, lower=1, upper=100):
     """Return a random whole number (nonzero integer)."""
     if lower < 1:
         raise ValueError(f"Numbers < 1 are not whole numbers")
-    rand = resolve_random_instance(rand)
-    return rand.randint(lower, upper)
+    return random.randint(lower, upper)
 
 
-def random_integer(*, lower=-100, upper=100, rand=None):
+def random_integer(*, lower=-100, upper=100):
     """Return a random integer."""
-    rand = resolve_random_instance(rand)
-    return rand.randint(lower, upper)
+    return random.randint(lower, upper)
 
 
 def random_float(*, lower=-sys.float_info.max, upper=sys.float_info.max, rand=None):
     """Return a random floating point number."""
     return rand.uniform(lower, upper)
-
-
-def resolve_random_instance(rand):
-    """Return the input argument or the global random instance if the input
-    argument is None"""
-    if rand is None:
-        rand = random._inst
-    return rand
