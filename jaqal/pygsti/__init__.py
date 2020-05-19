f8055c1a2c36b07360c7f246ed67f0f548f31d55
Kenneth Rudinger <kmrudin@sandia.gov>
2020-05-19 14:29:08 -0600

    Add finite-count emulation.

--- jaqal/pygsti/__init__.py
+++ jaqal/pygsti/__init__.py
@@ -1,10 +1,14 @@
 from .frontend import pygsti_circuit_from_code, pygsti_label_from_statement
 from .pygstimodel import build_noiseless_native_model
-from .forward_simulation import forward_simulate_circuit
+from .forward_simulation import (
+    forward_simulate_circuit,
+    forward_simulate_circuit_counts,
+)
 
 __all__ = [
     "pygsti_circuit_from_code",
     "pygsti_label_from_statement",
     "build_noiseless_native_model",
     "forward_simulate_circuit",
+    "forward_simulate_circuit_counts",
 ]
--- jaqal/pygsti/forward_simulation.py
+++ jaqal/pygsti/forward_simulation.py
@@ -19,3 +19,25 @@ def forward_simulate_circuit(
     pygsti_circuit = pygsti_circuit_from_code(circuit)
     probs = model.probs(pygsti_circuit)
     return probs
+
+
+def forward_simulate_circuit_counts(
+    jaqal_circ,
+    N,
+    model=None,
+    qubit_label_func=lambda qidx: "q[{}]".format(qidx),
+    tol=1e-6,
+):
+    probs = forward_simulate_circuit(
+        jaqal_circ, model=model, qubit_label_func=qubit_label_func
+    )
+    for item in probs.items():
+        if item[1] < -tol:
+            probs[item[0]] = 0
+        elif item[1] > 1 + tol:
+            probs[item[0]] = 1
+    counts = np.random.multinomial(N, list(probs.values()))
+    counts_dict = {}
+    for key_idx, key in enumerate(probs.keys()):
+        counts_dict[key] = counts[key_idx]
+    return counts_dict
