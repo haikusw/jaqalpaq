594ef904220a4f49bbe46a0cd67c68daf3b4a06c
Jay <jayvand@sandia.gov>
2020-06-09 20:36:22 -0600

    Added algorithm to normalize gate blocks

---
+++ jaqalpaq/core/algorithm/unit_timing.py
@@ -0,0 +1,113 @@
+"""Normalize gates in blocks under the assumption that they all have
+unitary timing (or are padded to be so)."""
+
+from itertools import zip_longest
+
+from jaqalpaq import JaqalError
+from jaqalpaq.core.visitor import Visitor
+import jaqalpaq.core as core
+
+
+def normalize_blocks_with_unitary_timing(circuit):
+    """Normalize the given circuit to contain only parallel blocks. This
+    is possible by assuming that all gates run in parallel run in one unit
+    of time.
+
+    This does not expand loops, macros, lets, or maps.
+
+    :param circuit: The circuit to normalize.
+
+    :returns: A new, normalized circuit. Although the circuit will be
+    new, it may share structure with the input circuit, thus the input
+    should not be changed.
+
+    """
+
+    visitor = BlockNormalizer()
+    return visitor.visit(circuit)
+
+
+class BlockNormalizer(Visitor):
+
+    def visit_default(self, obj):
+        """By default we leave all objects alone. Note that the object is not
+        copied."""
+        return obj
+
+    def visit_ScheduledCircuit(self, circuit):
+        """Return a new Circuit with the same metadata and normalized
+        gates."""
+
+        new_circuit = core.circuit.ScheduledCircuit(native_gates=circuit.native_gates)
+        new_circuit.constants.update(circuit.constants)
+        new_circuit.macros.update(circuit.macros)
+        new_circuit.registers.update(circuit.registers)
+        new_circuit.body.statements.extend(self.visit(circuit.body).statements)
+        return new_circuit
+
+    def visit_BlockStatement(self, obj: core.BlockStatement):
+        """Normalize a block by first normalizing every statement or block
+        inside it, If this is a parallel block, it is converted to a
+        serial block with one or more serial blocks. If it is a serial
+        block, it is passed up with its normalized statements and any
+        subblocks that are sequential expanded.
+
+        """
+
+        visited_statements = [self.visit(stmt) for stmt in obj.statements]
+
+        if obj.parallel:
+            new_statements = []
+            for parallel_chunk in self.iter_chunk_blocks(visited_statements):
+                if len(parallel_chunk) == 1:
+                    block, = parallel_chunk
+                else:
+                    block = core.BlockStatement(parallel=True, statements=parallel_chunk)
+                new_statements.append(block)
+        else:
+            new_statements = list(self.iter_unroll_blocks(visited_statements))
+        new_block = core.BlockStatement(statements=new_statements)
+        return new_block
+
+    def iter_chunk_blocks(self, statements):
+        """Iterate over statements in a parallel block and return chunks
+        consisting of the next statement from within each of those blocks
+        (treating a gate as a block of 1 statement)."""
+        iterators = [UnrollIterator().visit(stmt) for stmt in statements]
+        for ch in zip_longest(*iterators):
+             non_none = list(filter(lambda x: x is not None, ch))
+             chunk = []
+             for stmt in non_none:
+                 if isinstance(stmt, core.BlockStatement):
+                     assert stmt.parallel, "Normalization Failed"
+                     chunk.extend(stmt.statements)
+                 else:
+                     chunk.append(stmt)
+                 if isinstance(stmt, core.LoopStatement):
+                     raise JaqalError("A Loop is embedded somewhere within a parallel block. This is legal Jaqal, but cannot be handled here. Unroll loops first.")
+                 
+             yield chunk
+
+    def iter_unroll_blocks(self, statements):
+        for stmt in statements:
+            yield from UnrollIterator().visit(stmt)
+
+
+class UnrollIterator(Visitor):
+    """Unroll a block or single gate into a sequence of gates. The
+    resulting sequence will have no sequential blocks but perhaps
+    parallel blocks.
+
+    """
+
+    def visit_default(self, obj):
+        yield obj
+
+    def visit_BlockStatement(self, obj):
+        if obj.parallel:
+            # This is ok in iter_unroll_blocks but would be an error
+            # in iter_chunk_blocks.
+            yield obj
+        else:
+            for stmt in obj.statements:
+                yield stmt
---
+++ test/core/algorithm/test_unit_timing.py
@@ -0,0 +1,57 @@
+import unittest
+
+from jaqalpaq import JaqalError
+from jaqalpaq.parser import parse_jaqal_string
+from jaqalpaq.core.algorithm.unit_timing import normalize_blocks_with_unitary_timing
+
+class UnitTimingTester(unittest.TestCase):
+
+    def test_single_gate(self):
+        test = "foo 1 2 3"
+        exp = "foo 1 2 3"
+        self.run_test(test, exp)
+
+    def test_sequential_block(self):
+        test = "{foo}"
+        exp = "foo"
+        self.run_test(test, exp)
+
+    def test_parallel_block(self):
+        test = "<foo|bar>"
+        exp = "<foo|bar>"
+        self.run_test(test, exp)
+
+    def test_top_level_loop(self):
+        test = "loop 5 {foo; bar}"
+        exp = "loop 5 {foo; bar}"
+        self.run_test(test, exp)
+
+    def test_seq_in_par_in_seq(self):
+        test = "{g0;g1;<p0|p1|p2|{q0;q1}>;g2}"
+        exp = "g0;g1;<p0|p1|p2|q0>;q1;g2"
+        self.run_test(test, exp)
+
+    def test_par_in_seq_in_par_1(self):
+        test = "<p0|p1|{<g0|g1>;g2}>"
+        exp = "<p0|p1|g0|g1>;g2"
+        self.run_test(test, exp)
+
+    def test_par_in_seq_in_par_2(self):
+        test = "<p0|{<g0|g1>;g2}|{<a0|a1>;a2}|p1>"
+        exp = "<p0|g0|g1|a0|a1|p1>;<g2|a2>"
+        self.run_test(test, exp)
+
+    def test_reject_nested_loop(self):
+        test = "<{loop 5 {}}>"
+        with self.assertRaises(JaqalError):
+            normalize_blocks_with_unitary_timing(parse_jaqal_string(test))
+
+    def run_test(self, test, exp):
+        exp_result = parse_jaqal_string(exp)
+        test_parsed = parse_jaqal_string(test)
+        act_result = normalize_blocks_with_unitary_timing(test_parsed)
+        self.assertEqual(exp_result, act_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
