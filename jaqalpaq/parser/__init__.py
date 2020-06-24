7d4259235cd1c51f42052881ff62b517af5817ce
Jay <jayvand@sandia.gov>
2020-06-24 16:55:21 -0600

    Defined the public API for jaqalpaq.parser

--- jaqalpaq/parser/__init__.py
+++ jaqalpaq/parser/__init__.py
@@ -1,5 +1,4 @@
 # Parse Jaqal text into core objects.
 from .parser import parse_jaqal_file, parse_jaqal_string, Option, JaqalParseError
 
-# A lower-level, slightly obsolete but not yet deprecated interface to the parser.
-from .interface import Interface
+__all__ = ["parse_jaqal_file", "parse_jaqal_string", "Option", "JaqalParseError"]
