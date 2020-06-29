5466aca4e31bc0e018eef33c1ce4a9899914495a
Kenneth Rudinger <kmrudin@sandia.gov>
2020-06-29 09:44:56 -0600

    Fix errors in examples/jaqal/single_qubit_gst.jaqal.

--- examples/jaqal/single_qubit_gst.py
+++ examples/jaqal/single_qubit_gst.py
@@ -8,7 +8,7 @@
         "macro",
         "F3",
         "qubit",
-        ("sequential_block", ("gate", "Sx", "qubit"), ("gate", "Sy", "qubit")),
+        ("sequential_block", ("gate", "Sx", "qubit"), ("gate", "Sx", "qubit")),
     ),
     (
         "macro",
@@ -39,7 +39,7 @@
         "macro",
         "G3",
         "qubit",
-        ("sequential_block", ("gate", "Sx", "qubit"), ("gate", "Sy", "qubit")),
+        ("sequential_block", ("gate", "Sx", "qubit"), ("gate", "Sx", "qubit")),
     ),
     (
         "macro",
