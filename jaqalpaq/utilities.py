9b38d846e3ff9e34334682656cabefc85cc981d5
jayvand <jayvand@sandia.gov>
2020-07-02 08:30:38 -0600

    Added type information to RESERVED_WORDS

--- jaqalpaq/utilities.py
+++ jaqalpaq/utilities.py
@@ -1,2 +1,2 @@
-#: Words that cannot be used as names of constants, macros, or registers in Jaqal code.
+#: A list of words that cannot be used as names of constants, macros, or registers in Jaqal code.
 RESERVED_WORDS = ["let", "map", "register", "macro", "loop", "version"]
