587c3aa096d39aff57673d4a482a0bcfd292c854
Antonio Russo <arusso@sandia.gov>
2020-08-03 18:56:28 -0600

    Update setup infrastructure

--- setup.py
+++ setup.py
@@ -49,16 +49,19 @@ else:
 
 name = "JaqalPaq"
 description = "Python tools for Jaqal"
-version = "1.0"
+version = "1.0.0b0"
 
 setup(
     name=name,
     description=description,
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     version=version,
     author="Benjamin C. A. Morrison, Jay Wesley Van Der Wall, Daniel Lobser, Antonio Russo, Kenneth Rudinger, Peter Maunz",
     author_email="qscout@sandia.gov",
     packages=find_packages(include=["jaqalpaq", "jaqalpaq.*"]),
     package_dir={"": "."},
+    package_data={"jaqalpaq.parser": ["jaqal_grammar.lark"],},
     install_requires=["lark-parser"],
     extras_require={
         "tests": ["pytest"],
@@ -69,10 +72,10 @@ setup(
     platforms=["any"],
     url="https://qscout.sandia.gov",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Physics",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
