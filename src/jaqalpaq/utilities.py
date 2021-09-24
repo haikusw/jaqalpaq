01c11e6e6a8d276d681237d2a7ab453644fdae42
Jay <jayvand@sandia.gov>
2021-09-24 14:23:06 -0600

    Added keywords from import-type statements to RESERVED_WORDS

--- src/jaqalpaq/utilities.py
+++ src/jaqalpaq/utilities.py
@@ -2,4 +2,15 @@
 # Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
 # certain rights in this software.
 #: A list of words that cannot be used as names of constants, macros, or registers in Jaqal code.
-RESERVED_WORDS = ["let", "map", "register", "macro", "loop", "version"]
+RESERVED_WORDS = [
+    "let",
+    "map",
+    "register",
+    "macro",
+    "loop",
+    "version",
+    "import",
+    "usepulses",
+    "from",
+    "as",
+]
