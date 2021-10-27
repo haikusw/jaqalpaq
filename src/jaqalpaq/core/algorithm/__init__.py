78f5c1d0d351d37352b174847e4a1eca970012f7
Jay <jayvand@sandia.gov>
2021-10-27 13:29:42 -0600

    Added expand_subcircuits to core.algorithm api

--- src/jaqalpaq/core/algorithm/__init__.py
+++ src/jaqalpaq/core/algorithm/__init__.py
@@ -2,12 +2,14 @@
 # Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
 # certain rights in this software.
 from .expand_macros import expand_macros
+from .expand_subcircuits import expand_subcircuits
 from .unit_timing import normalize_blocks_with_unitary_timing
 from .used_qubit_visitor import get_used_qubit_indices
 from .fill_in_let import fill_in_let
 
 __all__ = [
     "expand_macros",
+    "expand_subcircuits",
     "normalize_blocks_with_unitary_timing",
     "get_used_qubit_indices",
     "fill_in_let",
