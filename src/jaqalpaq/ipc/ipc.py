d15e2aaf299f0f52cab333dfaf1fa47f0ad8b8b7
Antonio Russo <arusso@sandia.gov>
2022-11-08 11:39:07 -0700

    Clarify API status of jaqalpaq.ipc
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- src/jaqalpaq/ipc/ipc.py
+++ src/jaqalpaq/ipc/ipc.py
@@ -1,6 +1,9 @@
 # Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
 # Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
 # certain rights in this software.
+"""
+(internal) All members of this module should be considered internal API.
+"""
 
 import os
 import socket
